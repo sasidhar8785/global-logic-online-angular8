b3c61b0319a4c9bf804f2b019a4e6cb186daeb99
kpaturi <kpaturi@ipromoteu.com>
2020-04-11 11:08:10 +0530

    Integrated Header

--- flipzon/src/app/app.module.ts
+++ flipzon/src/app/app.module.ts
@@ -3,9 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+//step 1 import Header Component
+import { HeaderComponent } from './header/header.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //include Header Component to use it in the application.
+    HeaderComponent
   ],
   imports: [
     BrowserModule
